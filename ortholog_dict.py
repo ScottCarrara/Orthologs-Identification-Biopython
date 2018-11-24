176e25431be08e5e33368e17cf8d79a6304cd653
SamSamhuns <sthapuskar@yahoo.com>
2018-11-24 13:23:02 -0500

    Lowered the E value thres to get results for the given xml files

--- ortholog_dict.py
+++ ortholog_dict.py
@@ -26,7 +26,7 @@ cd_parse = NCBIXML.parse(cd_file)
 dc_parse = NCBIXML.parse(dc_file)
 
 # Setting the E_VALUE threshold
-E_VALUE_THRESH = 10e-20
+E_VALUE_THRESH = 1
 
 # using data structure Dictionary / Hash tables
 # This is a dictionary of lists ( But the lists do not affect the Big Oh runtime )
